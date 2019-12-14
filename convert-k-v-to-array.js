6117daa3ddf858306386a301c55cbbeb7332f583
Rory Murray <rory.christian.murray@gmail.com>
2019-12-14 13:06:00 -0500

    Get keys from object method

--- convert-k-v-to-array.js
+++ convert-k-v-to-array.js
@@ -11,3 +11,24 @@
 //   dislikes: 3,
 //   followers: 10
 // }) ➞ [["likes", 2], ["dislikes", 3], ["followers", 10]]
+
+o1 = {
+    D: 1,
+    B: 2,
+    C: 3
+}
+
+function objectToArray(obj) {
+    console.log("in objectToArray")
+    console.log("\nobj : ", obj)
+
+    objKeys = Object.keys(objs)
+	
+	console.log("\nobjKeys : ", objKeys)
+}
+
+
+t1 = objectToArray(o1)
+
+t1
+console.log(t1)
