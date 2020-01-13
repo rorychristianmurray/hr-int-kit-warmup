d487f0113312cff95bf302b642c209676075d08e
Rory Murray <rory.christian.murray@gmail.com>
2020-01-13 18:12:43 -0500

    Largest swap WIP

---
+++ largest_swap.js
@@ -0,0 +1,18 @@
+function largestSwap(num) {
+	// check digits
+	// if currDigits >= swapDigits
+		// return true
+	// else return false
+	
+	// reverse a string?
+	
+	// turn number to string
+	let stringyNum = `${num}`
+	console.log("stringyNum : ", stringyNum)
+	console.log("stringyNum typeof : ", typeof stringyNum)
+	
+	// add to an array
+	// add to new array in reverse order
+	// combine into an integer
+	
+}
